afaf8b04af0164154b602de7caa1ea33e423107c
Michael Strahl <michaelstrahl.sm@gmail.com>
2023-12-27 17:50:00 -0300

    refresh token

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,23 @@ import { orgsRoutes } from './http/controllers/orgs/routes'
 import { ZodError } from 'zod'
 import { env } from './env'
 import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
 import { petsRoutes } from './http/controllers/pets/routes'
 
 export const app = fastify()
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
 })
+
+app.register(fastifyCookie)
 
 app.register(orgsRoutes)
 app.register(petsRoutes)
